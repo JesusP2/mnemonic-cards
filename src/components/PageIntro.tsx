94067ee72755d2cdc684741308957c6b3576e49e
JesusP2 <jesus_perez99@hotmail.com>
2022-05-18 01:19:01 -0500

    Added styles to the page intro component

--- src/components/PageIntro.tsx
+++ src/components/PageIntro.tsx
@@ -1,8 +1,8 @@
 export default function NoDeckView() {
     return (
         <div className='w-full'>
-            <h1>Anki cards clone</h1>
-            <p>Short personal project for creating for creating cards and decks to make learning things easier.</p>
+            <h1 className="text-white font-acme text-4xl text-center mt-8">NIMONIKKU</h1>
+            <p className='text-center mt-4'>Short personal project for creating for creating cards and decks to make learning things easier.</p>
         </div>
     )
 }
